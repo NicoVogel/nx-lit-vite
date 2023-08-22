599b421f3ee0ef73d948f3d992c5d79cc8de8422
Nico Vogel <nico-vogel97@outlook.de>
2023-08-22 08:55:26 +0000

    fixup gloablly

--- tools/scripts/vite-shared.ts
+++ tools/scripts/vite-shared.ts
@@ -1,4 +1,19 @@
-import {viteDefineConfig} from './vite-define.js';
+import {execSync} from 'node:child_process';
+
+function getShortHash() {
+  try {
+    return execSync('git describe --always').toString().trim();
+  } catch (error) {
+    return 'No git commit hash available';
+  }
+}
+
+function viteDefineConfig(packageJson: {version: string}) {
+  return {
+    'import.meta.env.COMMIT': `'${getShortHash()}'`,
+    'import.meta.env.VERSION': `'${packageJson.version}'`,
+  };
+}
 
 export function getViteSharedConfig(packageJson: {version: string}) {
   return {
